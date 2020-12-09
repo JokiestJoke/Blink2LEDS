dcce2924dd949a5854b2820aa12b33665829669a
JokiestJoke <mdoghram0@gmail.com>
2020-12-09 11:21:40 -0500

    blinking 2 leds with a few second blinks

---
+++ Blink2LEDS.ino
@@ -0,0 +1,25 @@
+//Sets up the pins for arduino nano
+//Pin 13 as led
+int led = 13;
+// Pin 12 as ledTwo
+int ledTwo = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  // Initializes the pins as outputs
+  pinMode(led, OUTPUT);
+  pinMode(ledTwo, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH); // Turns voltage on High
+  delay(300);  // waits 2 seconds
+  digitalWrite(led, LOW);
+  delay(100);
+  digitalWrite(ledTwo, HIGH); // Turns voltage on High
+  delay(300);  // waits 2 seconds
+  digitalWrite(ledTwo, LOW);
+  delay(100);
+  
+}
